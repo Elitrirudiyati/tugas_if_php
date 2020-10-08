e487e98e915cf1d3d4717c46cbc2bf2dcc82563e
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-08 12:00:13 +0700

    Add files via upload

---
+++ tugas_if_php.php
@@ -0,0 +1,26 @@
+<?php
+//Buatlah kode program yang bisa membedakan angka genap dengan angka ganjil. Misalkan variabel $a berisi 50, akan tampil “50 adalah angka genap”. Jika $a berisi 7, maka akan tampil “7 adalah angka ganjil”. Tips: gunakan operator modulus (%) untuk membedakan bilangan genap dan ganjil.
+
+
+$nilai_a = 50;
+
+if ($nilai_a%2 == 0) {
+echo" ".$nilai_a." adalah angka genap";
+}else{
+echo" ".$nilai_a." adalah angka ganjil";
+}
+
+
+
+$nilai_b = 7;
+
+if ($nilai_b%2 == 0) {
+echo" ".$nilai_b." adalah angka genap";
+}else{
+echo" ".$nilai_b." adalah angka ganjil";
+}
+
+
+
+?>
+
